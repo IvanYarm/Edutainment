2fb84990c59d97318604579874cfd52c1e1339d7
Ivan <ivan.yarmoliuk@gmail.com>
2023-05-17 18:58:31 -0700

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Ivan Yarmoliuk on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
